c04cc1c57516e2ed3170632ddab67046cefd77d5
Jahnavi Sai <128013017@sastra.ac.in>
2026-06-25 15:36:37 +0530

    Try port 465 SSL for email instead of 587 TLS

--- app/__init__.py
+++ app/__init__.py
@@ -15,10 +15,11 @@ def create_app():
     
     # Flask-Mail configuration
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_PORT'] = 465
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+
     
     mail.init_app(app)
     app.register_blueprint(auth.bp)
